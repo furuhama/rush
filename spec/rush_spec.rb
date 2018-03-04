2a4869721b909eff0402a3d313e562d6c53b3754
furuhama <furuhama@users.noreply.github.com>
2018-03-04 13:17:13 +0900

    Edit spec for readability

--- spec/rush_spec.rb
+++ spec/rush_spec.rb
@@ -7,21 +7,25 @@ describe 'Rush' do
 
     context 'normal input' do
       let(:string) { '(hoge)' }
+
       it { is_expected.to eq ['(', 'hoge', ')'] }
     end
 
     context 'input with extra spaces' do
       let(:string) { '( hoge )' }
+
       it { is_expected.to eq ['(', 'hoge', ')'] }
     end
 
-    context 'many ( & )' do
+    context 'many ( & ), and extra spaces' do
       let(:string) { '( (haskell)((     (( () ruby' }
+
       it { is_expected.to eq ['(', '(', 'haskell', ')', '(', '(', '(', '(', '(', ')', 'ruby'] }
     end
 
     context 'other symbols' do
       let(:string) { ':^@$"%^' }
+
       it { is_expected.to eq [':^@$"%^' ] }
     end
   end
@@ -29,19 +33,24 @@ describe 'Rush' do
   describe '#read_tokens' do
     subject { read_tokens tokens }
 
-    context 'length == 0' do
-      let(:tokens) { [] }
-      it { expect { subject }.to raise_error(SyntaxError) }
-    end
+    context 'raise Syntax Error' do
+      context 'length == 0' do
+        let(:tokens) { [] }
 
-    context 'tokens just have "("' do
-      let(:tokens) { [')'] }
-      it { expect { subject }.to raise_error(SyntaxError) }
-    end
+        it { expect { subject }.to raise_error(SyntaxError) }
+      end
 
-    context 'tokens start ")"' do
-      let(:tokens) { [')', '(', 'hoge', ')', ')'] }
-      it { expect { subject }.to raise_error(SyntaxError) }
+      context 'tokens just have "("' do
+        let(:tokens) { [')'] }
+
+        it { expect { subject }.to raise_error(SyntaxError) }
+      end
+
+      context 'tokens start ")"' do
+        let(:tokens) { [')', '(', 'hoge', ')', ')'] }
+
+        it { expect { subject }.to raise_error(SyntaxError) }
+      end
     end
   end
 end
